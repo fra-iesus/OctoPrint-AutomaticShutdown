3d0807be9e0fc8952c6f19500940725783ef3ece
DeltaMaker <info@deltamaker.com>
2017-05-01 13:52:41 -0400

    1.4

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Nicanor Romero Venier"
 plugin_author_email = "nicanor.romerovenier@bq.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/OctoPrint/OctoPrint-AutomaticShutdown"
+plugin_url = "https://github.com/DeltaMaker/OctoPrint-AutomaticShutdown"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
